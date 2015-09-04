5b95aa7ce9bbefda769733a2ea308a14d9479f54
inei <iancnei@gmail.com>
2015-09-04 12:43:04 -0700

    connect libraries with front end

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var path = require('path');
 
 var app = express();
-
 app.use(bodyParser.urlencoded({extended: true}));
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
+
+var views = path.join(process.cwd(), 'views');
 
 app.get("/", function(req, res)
 {
-	res.send("（´・ω・｀）");
+	res.sendFile(path.join(views, "home.html"));
 });
 
 var listener = app.listen(3000, function()
